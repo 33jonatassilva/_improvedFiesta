b8f8907e4df5f48b2d815963db9d540fa28f54ca
33jonatassilva <jonatasbsilva1@outlook.com>
2024-11-07 13:16:38 -0300

    Clients update

--- _projecthelped/EasyManage/EasyManage/obj/Debug/net8.0/EasyManage.AssemblyInfo.cs
+++ _projecthelped/EasyManage/EasyManage/obj/Debug/net8.0/EasyManage.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasyManage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5388b890fe5086eef0789e3837fb5168b3e15436")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7029a88c379e9870eef02b8f315d6255dd08bdeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasyManage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasyManage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
